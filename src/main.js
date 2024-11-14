3bcb8d17bcacd3bf67c3604c87627a6f1ac8d324
Ivan Amoshyi <amoshyiivan@gmail.com>
2024-11-14 19:26:20 +0200

    styled select, added proper error handling

--- src/main.js
+++ src/main.js
@@ -1,21 +1,45 @@
 import { fetchBreeds, fetchCatByBreed } from './js/api';
+import Swal from 'sweetalert2';
 
 const selectEl = document.querySelector('.breed-form_select');
 const catInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+
+loader.classList.remove('is-hidden');
 
 fetchBreeds()
   .then(({ data }) => renderSelectOptions(data))
-  .catch(e => console.log(e));
+  .catch(e => {
+    Swal.fire({
+      title: 'Error!',
+      text: e.message,
+      icon: 'error',
+      confirmButtonText: 'Ok',
+    });
+  })
+  .finally(() => {
+    loader.classList.add('is-hidden');
+    selectEl.classList.remove('is-hidden');
+  })
 
 selectEl.addEventListener('change', (e) => {
-  catInfo.innerHTML = '';
+  catInfo.innerHTML = ``;
+  loader.classList.remove('is-hidden');
   const breedId = e.target.value;
 
   fetchCatByBreed(breedId)
     .then(({ data }) => {
+      loader.classList.add('is-hidden');
       renderCatInfo(data[0]);
     })
-    .catch(e => console.log(e));
+    .catch(e => {
+      Swal.fire({
+        title: 'Error!',
+        text: e.message,
+        icon: 'error',
+        confirmButtonText: 'Ok',
+      });
+    });
 })
 
 function renderSelectOptions(arr) {
