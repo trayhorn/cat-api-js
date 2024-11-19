74997b134efd7bcf55d183e80c44336f3e6ac14b
trayhorn <86042026+trayhorn@users.noreply.github.com>
2024-11-19 16:47:37 +0000

    Deploying to gh-pages from @ trayhorn/cat-api-js@6045a3e955e868b9bb756ac6781667ef6205ced5 🚀

--- cat-gallery.js
+++ cat-gallery.js
@@ -1,2 +1,4 @@
-import{f as i}from"./assets/api-BsiVDZ6G.js";import{S as c}from"./assets/vendor-B0ghNh3e.js";const t=document.querySelector(".breed-form_select");i().then(({data:e})=>s(e)).catch(e=>{c.fire({title:"Error!",text:e.message,icon:"error",confirmButtonText:"Ok"})}).finally(()=>{t.classList.remove("is-hidden")});function s(e){const o=e.map(({id:r,name:n})=>`<option value="${r}">${n}</option>`).join("");t.innerHTML=o}
+import{f as o,b as c}from"./assets/api-BM8oayT1.js";import{S as l}from"./assets/vendor-B0ghNh3e.js";const n=document.querySelector(".breed-form_select"),i=document.querySelector(".cat-gallery");o().then(({data:e})=>s(e)).catch(e=>{l.fire({title:"Error!",text:e.message,icon:"error",confirmButtonText:"Ok"})}).finally(()=>{n.classList.remove("is-hidden")});n.addEventListener("change",e=>{const t=e.target.value;c(t).then(r=>{i.innerHTML=r.data.map(({url:a})=>`<li class="cat-gallery_item">
+        <img class="cat-gallery_image" src="${a}" alt="">
+      </li>`).join("")})});function s(e){const t=e.map(({id:r,name:a})=>`<option value="${r}">${a}</option>`).join("");n.innerHTML=t}
 //# sourceMappingURL=cat-gallery.js.map
--- cat-search.js
+++ cat-search.js
@@ -1,11 +1,11 @@
-import{f as d,a as l}from"./assets/api-BsiVDZ6G.js";import{S as o}from"./assets/vendor-B0ghNh3e.js";const s=document.querySelector(".breed-form_select"),c=document.querySelector(".cat-info"),r=document.querySelector(".loader");r.classList.remove("is-hidden");d().then(({data:e})=>m(e)).catch(e=>{o.fire({title:"Error!",text:e.message,icon:"error",confirmButtonText:"Ok"})}).finally(()=>{r.classList.add("is-hidden"),s.classList.remove("is-hidden")});s.addEventListener("change",e=>{c.innerHTML="",r.classList.remove("is-hidden");const n=e.target.value;l(n).then(({data:t})=>{r.classList.add("is-hidden"),f(t[0])}).catch(t=>{o.fire({title:"Error!",text:t.message,icon:"error",confirmButtonText:"Ok"})})});function m(e){const n=e.map(({id:t,name:i})=>`<option value="${t}">${i}</option>`).join("");s.innerHTML=n}function f(e){const{temperament:n,name:t,description:i}=e.breeds[0],a=`
+import{f as d,a as l}from"./assets/api-BM8oayT1.js";import{S as m}from"./assets/vendor-B0ghNh3e.js";const i={error(e){return m.fire({title:"Error!",text:e.message,icon:"error",confirmButtonText:"Ok",didClose:()=>{location.reload()}})}},s=document.querySelector(".breed-form_select"),f=document.querySelector(".custom-arrow"),c=document.querySelector(".cat-info"),n=document.querySelector(".loader");n.classList.remove("is-hidden");d().then(({data:e})=>h(e)).catch(e=>i.error(e)).finally(()=>{n.classList.add("is-hidden"),s.classList.remove("is-hidden"),f.classList.remove("is-hidden")});s.addEventListener("change",u);function u(e){c.innerHTML="",n.classList.remove("is-hidden");const r=e.target.value;l(r).then(({data:t})=>{n.classList.add("is-hidden"),p(t[0])}).catch(t=>i.error(t))}function h(e){const r=e.map(({id:t,name:o})=>`<option value="${t}">${o}</option>`).join("");s.innerHTML=r}function p(e){const{temperament:r,name:t,description:o}=e.breeds[0],a=`
         <div>
           <img class="cat-info_image" src="${e.url}" alt="">
         </div>
         <div>
           <h2 class="cat-info_name">${t}</h2>
-          <p class="cat-info_temper">${n}</p>
-          <p class="cat-info_description">${i}</p>
+          <p class="cat-info_temper">${r}</p>
+          <p class="cat-info_description">${o}</p>
         </div>
       `;c.innerHTML=a}
 //# sourceMappingURL=cat-search.js.map
