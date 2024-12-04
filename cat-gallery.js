5a7e78f291684eebf6d29cf73f8756135df7ab37
trayhorn <86042026+trayhorn@users.noreply.github.com>
2024-12-04 17:27:51 +0000

    Deploying to gh-pages from @ trayhorn/cat-api-js@6c5430d3da9ab384263d3ccd80e36df18c1dd1f6 🚀

--- cat-gallery.js
+++ cat-gallery.js
@@ -1,14 +1,14 @@
-import{g as r,c as d,n as o,d as m}from"./assets/notifications-D1Rdp1Si.js";import"./assets/vendor-DCC95h-G.js";const n=document.querySelector(".fav-gallery"),c=document.querySelector(".loader");c.classList.remove("is-hidden");g();async function g(){try{const{data:a}=await r(),e=a.map(i=>i.image_id),t=await Promise.all(e.map(async i=>{const{data:s}=await d(i);return s}));n.innerHTML=v(t),n.addEventListener("click",u)}catch(a){o.error(a)}finally{c.classList.add("is-hidden")}}function v(a){return a.map(({url:e,id:t})=>`<li class="fav-gallery_item">
+import{g as o,n as r,d as n}from"./assets/notifications-DhPoR_Xn.js";import"./assets/vendor-DCC95h-G.js";const i=document.querySelector(".fav-gallery"),s=document.querySelector(".loader");s.classList.remove("is-hidden");c();async function c(){try{const{data:e}=await o();console.log(e),i.innerHTML=l(e),i.addEventListener("click",g)}catch(e){r.error(e)}finally{s.classList.add("is-hidden")}}function l(e){return e.map(({id:a,image:{url:t}})=>`<li class="fav-gallery_item">
               <img
                 class="fav-gallery_image"
-                src="${e}" alt=""
+                src="${t}" alt=""
               >
               <button class="fav-image_button">
                 <img 
                   class="fav-image_icon"
-                  data-image-id="${t}"
+                  data-image-id="${a}"
                   src="./img/cancel-circle.svg" alt=""
                 >
               </button>
-          </li>`).join("")}async function u(a){if(!a.target.classList.contains("fav-image_icon"))return;const e=a.target.dataset.imageId,{data:t}=await r(),{id:i}=t.find(l=>l.image_id===e);m(i),a.target.closest(".fav-gallery_item").remove(),o.removedFromFavorites()}
+          </li>`).join("")}async function g(e){if(!e.target.classList.contains("fav-image_icon"))return;const a=e.target.dataset.imageId;n(a),e.target.closest(".fav-gallery_item").remove(),r.removedFromFavorites()}
 //# sourceMappingURL=cat-gallery.js.map
