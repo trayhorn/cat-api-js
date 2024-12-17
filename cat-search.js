2070da67620ae932a7678631e50887ce47bd5ca0
trayhorn <86042026+trayhorn@users.noreply.github.com>
2024-12-17 15:35:46 +0000

    Deploying to gh-pages from @ trayhorn/cat-api-js@005b46d2ec83d9e8cf7f9989114b1014170b61e9 🚀

--- cat-search.js
+++ cat-search.js
@@ -1,23 +1,23 @@
-import{f,n,a as h,b as v,g as u,s as p}from"./assets/notifications-CaSNLay6.js";import{S as y}from"./assets/vendor-S7NC7oBj.js";const d=document.querySelector(".breed-form_select"),o=document.querySelector(".cat-gallery"),l=document.querySelector(".cat-info"),r=document.querySelector(".loader");r.classList.remove("is-hidden");f().then(({data:e})=>L(e)).catch(e=>n.error(e)).finally(()=>{r.classList.add("is-hidden"),d.classList.remove("is-hidden")});d.addEventListener("change",I);function L(e){const a=e.map(({id:t,name:s})=>`<option class="select-option" value="${t}">${s}</option>`).join("");d.insertAdjacentHTML("beforeend",a),new y({select:".breed-form_select"})}async function I(e){l.innerHTML="",o.innerHTML="",r.classList.remove("is-hidden");const a=e.target.value;try{const[t,s]=await Promise.all([h(a),v(a)]),{data:c}=t;_(c[0]);const{data:m}=s;o.innerHTML=m.filter(i=>i.width>i.height).map(({url:i,id:g})=>`<li class="cat-gallery_item">
+import{f as m,n as r,a as g,b as f,g as h,s as u}from"./assets/notifications-CaSNLay6.js";import{S as v}from"./assets/vendor-S7NC7oBj.js";const o=document.querySelector(".breed-form_select"),c=document.querySelector(".cat-gallery"),l=document.querySelector(".cat-info"),i=document.querySelector(".loader");i.classList.remove("is-hidden");m().then(({data:e})=>p(e)).catch(e=>r.error(e)).finally(()=>{i.classList.add("is-hidden"),o.classList.remove("is-hidden")});o.addEventListener("change",y);function p(e){const a=e.map(({id:t,name:s})=>`<option class="select-option" value="${t}">${s}</option>`).join("");o.insertAdjacentHTML("beforeend",a),new v({select:".breed-form_select"})}async function y(e){l.innerHTML="",c.innerHTML="",i.classList.remove("is-hidden");const a=e.target.value;try{const[t,s]=await Promise.all([g(a),f(a)]),{data:[n]}=t;console.log(n),l.innerHTML=L(n);const{data:d}=s;c.innerHTML=I(d),c.addEventListener("click",_)}catch{r.error(e)}finally{i.classList.add("is-hidden")}}function L(e){const{url:a,breeds:[{temperament:t,name:s,description:n}]}=e;return`
+        <div>
+          <img class="cat-info_image" src="${a}" alt="${s}">
+        </div>
+        <div>
+          <h2 class="cat-info_name">${s}</h2>
+          <p class="cat-info_temper">${t}</p>
+          <p class="cat-info_description">${n}</p>
+        </div>
+      `}function I(e){return e.filter(a=>a.width>a.height).map(({url:a,id:t})=>`<li class="cat-gallery_item">
             <div>
               <img
                 class="cat-gallery_image"
-                src="${i}" alt=""
+                src="${a}" alt=""
               >
               <img
-                data-image-id="${g}"
+                data-image-id="${t}"
                 class="cat-gallery_icon"
                 src="./img/heart.svg" alt="heart-icon"
               >
             </div>
-          </li>`).join(""),o.addEventListener("click",S)}catch{n.error(e)}finally{r.classList.add("is-hidden")}}function _(e){const{temperament:a,name:t,description:s}=e.breeds[0],c=`
-        <div>
-          <img class="cat-info_image" src="${e.url}" alt="">
-        </div>
-        <div>
-          <h2 class="cat-info_name">${t}</h2>
-          <p class="cat-info_temper">${a}</p>
-          <p class="cat-info_description">${s}</p>
-        </div>
-      `;l.innerHTML=c}async function S(e){if(!e.target.classList.contains("cat-gallery_icon"))return;const a=e.target.dataset.imageId;try{if((await u()).data.find(s=>s.image_id===a)){n.alreadyInFavariotes();return}await p(a),n.addedToFavorites()}catch(t){console.log(t)}}
+          </li>`).join("")}async function _(e){if(!e.target.classList.contains("cat-gallery_icon"))return;const{imageId:a}=e.target.dataset;try{if((await h()).data.find(s=>s.image_id===a)){r.alreadyInFavariotes();return}await u(a),r.addedToFavorites()}catch(t){console.log(t)}}
 //# sourceMappingURL=cat-search.js.map
